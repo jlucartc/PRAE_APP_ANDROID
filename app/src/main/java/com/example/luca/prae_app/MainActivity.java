b49876003fca780b744bf1b017e124ecee0eee4c
jlucartc <jlucartc@gmail.com>
2018-07-11 17:03:37 -0300

    Estudandando a API...

--- app/src/main/java/com/example/luca/prae_app/MainActivity.java
+++ app/src/main/java/com/example/luca/prae_app/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.luca.prae_app;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button noticias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void noticias(View view){
+
+        Intent intent = new Intent(this,NoticiasActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/luca/prae_app/NoticiasActivity.java
+++ app/src/main/java/com/example/luca/prae_app/NoticiasActivity.java
@@ -1,14 +1,50 @@
 package com.example.luca.prae_app;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.widget.CardView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NoticiasActivity extends Activity {
+
+    private Noticia[] noticias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticias);
+
+    }
+
+    public void buscarNoticias(){
+
+
+
+    }
+
+    public boolean isServicoAtivo(Class<?> classeServico){
+
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for(ActivityManager.RunningServiceInfo service : activityManager.getRunningServices(Integer.MAX_VALUE)){
+
+            if(classeServico.getName().equals(service.service.getClass())){
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+
     }
 
 }
