2194cea81d26bf0794a2bab6030e45dfa12d33d0
jlucartc <jlucartc@gmail.com>
2018-10-01 01:18:41 -0300

    adição em texto em 'Fale conosco'

--- app/src/main/java/com/example/luca/prae_app/menu_pages/fale_conosco/FaleConoscoActivity.java
+++ app/src/main/java/com/example/luca/prae_app/menu_pages/fale_conosco/FaleConoscoActivity.java
@@ -3,6 +3,8 @@ package com.example.luca.prae_app.menu_pages.fale_conosco;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
 
 import com.example.luca.prae_app.R;
 
@@ -13,7 +15,11 @@ public class FaleConoscoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fale_conosco);
 
-        getSupportActionBar().setTitle("Fale conosco");
+        getSupportActionBar().hide();
+
+        TextView faleConosco = findViewById(R.id.faleConosco);
+
+        faleConosco.setText(Html.fromHtml("Deseja entrar em contato com a PRAE? O nosso endereço de email é <b><i>case@ufc.br</i></b>. O nosso telefone é <b><i>3366-7444</i></b>. Nosso horário de funcionamento é das <b><i>8 da manhã</i></b> até as <b><i>17 horas</i></b>. Te esperamos aqui! :)"));
 
     }
 
